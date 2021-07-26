76ce29d231a621cdf0f9571d7498459e8d883621
sergeytodoriko <s.todorikos@gmail.com>
2021-07-26 23:43:38 +0300

    JAVAQA-21 (homework 1)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "";
+        String number = "6217813113990503";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
